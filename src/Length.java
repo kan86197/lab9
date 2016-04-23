f22fb054385df570da1df25327062226c4de88a5
bug bug <bug bug>
2016-04-23 15:10:02 +0700

    initial checkin

---
+++ src/Length.java
@@ -0,0 +1,31 @@
+
+public enum Length implements Unit{
+	METER("Meter", 1),
+	
+	KILOMETER("Kilometer", 1000.0),
+	
+	CENTIMETER("Centimeter", 0.01),
+	
+	NANOMETER("Nanometer", 0.0),
+	
+	MILE("Mile", 1609.344),
+	
+	FOOT("Foot", 0.3048);
+	
+	
+	private String name;
+	private double value;
+	
+	private Length(String name, double value){
+		this.name = name;
+		this.value = value;
+	}
+	
+	public double getValue(){
+		return value;
+	}
+	
+	public String toString(){
+		return name;
+	}
+}
---
+++ src/UnitConverter.java
@@ -0,0 +1,21 @@
+
+public class UnitConverter {
+	
+	/**
+	 * Use to convert a unit to another unit with the same value
+	 * @param amount is the value of the unit we want to convert
+	 * @param fromUnit the unit of the value we want to convert
+	 * @param toUnit the unit of the value after we converted it
+	 * @return the value of the double in new unit
+	 */
+	public double convert(double amount, Unit fromUnit, Unit toUnit){
+		return (amount * fromUnit.getValue()) / toUnit.getValue();
+	}
+	/**
+	 * Call to get the array of all the available units
+	 * @return an array of units from the Length enumerator
+	 */
+	public Unit[] getUnits(){
+		return Length.values();
+	}
+}
